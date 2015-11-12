08e52e50f7acd0a454d91aaa5eec756ca97fa570
yujue86 <mengxl2020@gmail.com>
2015-11-12 18:01:37 +0800

    fragment argument

--- app/src/main/java/com/example/dell/criminalintentpractice/CrimeActivity.java
+++ app/src/main/java/com/example/dell/criminalintentpractice/CrimeActivity.java
@@ -1,11 +1,15 @@
 package com.example.dell.criminalintentpractice;
 
 import android.support.v4.app.Fragment;
+import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected Fragment createFragment(){
-        return new CrimeFragment();
+    protected Fragment createFragment() {
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
